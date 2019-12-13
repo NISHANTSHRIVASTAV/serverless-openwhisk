947d71ebdeef906c1ada82b02d418a163e102fb2
nishant_shrivastav23 <nishant.shrivastav@esds.co.in>
2019-12-13 02:00:37 -0500

    Test git push

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function main(params) {
-  const name = params.name || 'ESDS';
+  const name = params.name || 'Iron Man';
   console.log('log', { payload: `Hello, ${name}` });
   return { payload: `Hello, ${name}!` };
 }
